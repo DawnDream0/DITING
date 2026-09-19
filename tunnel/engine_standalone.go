8925e0da37aef12aa355a85bef1ddf94e608ae89
黎 <80376109+DawnDream0@users.noreply.github.com>
2026-09-20 03:57:57 +0800

    Update engine_standalone.go

--- tunnel/engine_standalone.go
+++ tunnel/engine_standalone.go
@@ -57,7 +57,6 @@ func (e *Engine) serveDNS(w dns.ResponseWriter, r *dns.Msg, appOverride string, 
 
 	if appOverride != "" {
 		appName = appOverride
-		logf("DITING-DEBUG serveDNS domain=%s appName=%q appOverride=%q", domain, appName, appOverride)
 	} else if e.appResolver != nil {
 		if addr := w.RemoteAddr(); addr != nil {
 			srcPort := 0
